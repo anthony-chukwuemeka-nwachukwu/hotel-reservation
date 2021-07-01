91e32d17e5efc9d7f5aedf39d280d246726ffb26
Nwachukwu Anthony <nwachukwu.anthony2017@gmail.com>
2021-07-01 12:29:23 +0200

    add Reservation class

---
+++ src/model/Reservation.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Date;
+
+public class Reservation {
+    private final Customer customer;
+    private final IRoom room;
+    private final Date checkInDate;
+    private final Date checkOutDate;
+
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer:" + customer + ", Room:" + room + ", CheckInDate:" + checkInDate+ ", CheckOutDate:" + checkOutDate;
+    }
+}

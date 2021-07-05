135dd141aef5031ca328f7e59ad6cad1776501c1
Nwachukwu Anthony <nwachukwu.anthony2017@gmail.com>
2021-07-05 09:54:28 +0200

    override equals and hashCode

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.time.Instant;
 import java.util.Date;
+import java.util.Objects;
 
 public class Reservation {
     private final Customer customer;
@@ -13,6 +15,8 @@ public class Reservation {
             throw new IllegalArgumentException("All fields are required");
         }else if (checkInDate.after(checkOutDate) || checkInDate.equals(checkOutDate)){
             throw new IllegalArgumentException("Checkout date must be after checkin date");
+        }else if (checkInDate.before(Date.from(Instant.now()))){
+            throw new IllegalArgumentException("Checkin and checkout dates must not be in the past");
         }else{
             this.customer = customer;
             this.room = room;
@@ -35,6 +39,42 @@ public class Reservation {
 
     public Customer getCustomer(){
         return customer;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final Reservation other = (Reservation) obj;
+        if (!Objects.equals(this.checkInDate, other.checkInDate)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.checkOutDate, other.checkOutDate)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.room, other.room)) {
+            return false;
+        }
+
+        return Objects.equals(this.customer, other.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode (this.checkInDate);
+        hash = 53 * hash + Objects.hashCode (this.checkOutDate);
+        hash = 53 * hash + Objects.hashCode (this.room);
+        hash = 53 * hash + Objects.hashCode (this.customer);
+        return hash;
     }
 
     @Override

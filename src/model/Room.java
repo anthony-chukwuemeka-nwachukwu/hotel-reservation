97f87c5445cbd81678420d85b1a1c8f3990f636e
Nwachukwu Anthony <nwachukwu.anthony2017@gmail.com>
2021-07-05 09:54:00 +0200

    override equals and hashCode

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Room implements IRoom{
     private final String roomNumber;
     private final Double price;
@@ -39,6 +41,37 @@ public class Room implements IRoom{
     @Override
     public boolean isFree() {
         return price==0.0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final Room other = (Room) obj;
+        if (!Objects.equals(this.roomNumber, other.roomNumber)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.enumeration, other.enumeration)) {
+            return false;
+        }
+
+        return Math.round(this.price * Math.pow(10, 3)) / Math.pow(10, 3) == Math.round(other.price * Math.pow(10, 3)) / Math.pow(10, 3);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode (this.roomNumber);
+        hash = 31 * hash + Objects.hashCode (this.enumeration);
+        hash = 31 * hash + (int) (Double.doubleToLongBits(this.price));
+        return hash;
     }
 
     @Override
